47bcf54facf90d62ec828da8afdeb7466a0562bf
LiubomirM <liubomir.v.mladenov@gmail.com>
2017-05-18 17:21:26 +0300

    Add files via upload

---
+++ ExpressJS/Lab/handlers/home.js
@@ -0,0 +1,72 @@
+const url = require('url');
+const fs = require('fs');
+const path = require('path');
+const database = require('../config/database');
+const qs = require('querystring');
+
+module.exports = (req, res) => {
+    req.pathname = req.pathname || url.parse(req.url).pathname;
+
+    if (req.pathname === '/' && req.method === 'GET') {
+        
+        let filePath = path.normalize (path.join(__dirname, '../views/home/index.html'));
+
+
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.writeHead (404, {
+                    'Content-Type': 'text/plain'
+                })
+
+                res.write('404 not found');
+                res.end();
+                return;
+            }
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            })
+
+
+            let queryData = qs.parse(url.parse(req.url).query);
+
+            let products = database.products.getAll();
+
+            products.filter = (filterData) => {
+                let filter = filterData.toLowerCase();
+                let result = [];
+                for (let product of products) {
+                    let name = product.name.toLowerCase();
+                    let descr = product.description.toLowerCase();
+                    if ((name.indexOf(filter) !== -1) || (descr.indexOf(filter) !== -1)) {
+                        result.push(product);
+                    }
+                }
+                return result;
+            }
+            
+            if (queryData.query) {
+                products = products.filter(queryData.query);
+            }
+            console.log(products);
+
+            let content = '';
+            for (let product of products) {
+                content += 
+                `<div class="product-card">
+                    <img class="product-img" src="${product.image}">
+                    <h2>${product.name}</h2>
+                    <p>${product.description}</p>
+                </div>`
+            }
+
+            let html = data.toString().replace('{content}', content);
+
+            res.write(html);
+            res.end();
+        })
+    }else {
+        return true;
+    }
+}
